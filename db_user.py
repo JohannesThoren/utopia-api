32c52037875c9883445e18ca69e7d3207beb1ee5
johannes <johannes@lgjt.xyz>
2021-10-22 08:52:28 +0200

    poof

--- db_user.py
+++ db_user.py
@@ -1,54 +1,69 @@
 # Copyright (c) 2021 Johannes Thorén
-# 
+#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+import toml
 import bcrypt
 from uuid import uuid4, UUID
 
+settings = toml.load("settings.toml")
+
 def get_token(mongo, username, password):
-      db = mongo.db.users
-      user = db.find_one({"username": username})
+    db = mongo.db.users
+    user = db.find_one({"username": username})
 
-      if bcrypt.checkpw(password.encode("UTF-8"), user["password"]):
-            return {"response code": 200, "token":user["token"]}
-      else:
-            return {"response code": 401}
+    if bcrypt.checkpw(password.encode("UTF-8"), user["password"]):
+        return {"response code": 200, "token": user["token"]}
+    else:
+        return {"response code": 401}
 
-def create_user(mongo, username, password, email, image="http://localhost:3500/profilepic"):
-      db = mongo.db.users
-      
-      if not db.find_one({"username":username}):
-            password = bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
-            new_user = {"username": username, "email": email ,"password": password, "token": uuid4(), "id": uuid4(), "image": image}
-            db.insert(new_user)
-            return {"response code": 200, "username": username}
-      else:
-            return {"response code": 403}
+
+def create_user(mongo, username, password, email, image=str(settings["settings"]["url"]+"/profile_picture")):
+    db = mongo.db.users
+
+    if not db.find_one({"username": username}):
+        password = bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
+        new_user = {"username": username, "email": email, "password": password,
+                    "token": uuid4(), "id": uuid4(), "image": image}
+        db.insert(new_user)
+        return {"response code": 200, "username": username}
+    else:
+        return {"response code": 403}
+
+def delete_user(mongo, token):
+    db = mongo.db.users
+    user = db.find_one({"token": UUID(token)})
+    if user:
+        db.delete_one(user)
+        return {"response code": 200}
+    else:
+        return {"response code": 404}
 
 def get_user_by_name(mongo, username):
-      db = mongo.db.users
-      user = db.find_one({"username": username})
-      if user:
-            return {"response code": 200, "username": user["username"], "id": user["user_id"], "profile_picture": user["image"]}
-      else:
-            return {"response code": 404}
+    db = mongo.db.users
+    user = db.find_one({"username": username})
+    if user:
+        return {"response code": 200, "username": user["username"], "id": user["user_id"], "profile_picture": user["image"]}
+    else:
+        return {"response code": 404}
+
 
 def get_user_by_id(mongo, id):
-      db = mongo.db.users
-      user = db.find_one({"id": id})
-      if user:
-            return {"response code": 200, "username": user["username"], "id": user["user_id"], "profile_picture": user["image"]}
-      else:
-            return {"response code": 404}
+    db = mongo.db.users
+    user = db.find_one({"id": id})
+    if user:
+        return {"response code": 200, "username": user["username"], "id": user["user_id"], "profile_picture": user["image"]}
+    else:
+        return {"response code": 404}
+
 
 def get_user_by_token(mongo, token):
-      print(token)
-      db = mongo.db.users
-      token =  UUID(token)
-      user = db.find_one({"token": token})
-      if user:
-            return {"response code": 200, "username": user["username"], "id": user["id"], "profile_picture": user["image"]}
-      else:
-            return {"response code": 404}
+    print(token)
+    db = mongo.db.users
+    token = UUID(token)
+    user = db.find_one({"token": token})
+    if user:
+        return {"response code": 200, "username": user["username"], "id": user["id"], "profile_picture": user["image"]}
+    else:
+        return {"response code": 404}
--- routes_user.py
+++ routes_user.py
@@ -2,37 +2,48 @@ import re
 from flask import Flask, blueprints, request
 from flask_pymongo import PyMongo
 
-import db, db_user, db_threads, db_thread_posts
+import db
+import db_user
+import db_threads
+import db_thread_posts
 
 api_user = blueprints.Blueprint("api_user", __name__)
 
+
 @api_user.route('/user/get/token')
 def authenticate():
-      body = request.json
-      return db_user.get_token(db.mongo, body["username"], body["password"])
+    body = request.json
+    return db_user.get_token(db.mongo, body["username"], body["password"])
+
 
 @api_user.route('/user/new')
 def user_new():
-      body = request.json
-      return db_user.create_user(db.mongo, body["username"],body["password"],body["email"])
+    body = request.json
+    return db_user.create_user(db.mongo, body["username"], body["password"], body["email"])
+
 
 @api_user.route('/user/edit')
 def user_edit():
-      return
+    body = request.json
+    return body
+
 
 @api_user.route('/user/delete')
 def user_delete():
-      return
+    body = request.json
+    return db_user.delete_user(db.mongo, body["token"])
+
 
 @api_user.route('/user/get/username/<username>')
 def user_get_by_name(username):
-      return db_user.get_user_by_name(db.mongo, username)
+    return db_user.get_user_by_name(db.mongo, username)
+
 
 @api_user.route('/user/get/id/<user_id>')
 def user_get_by_id(user_id):
-      return db_user.get_user_by_id(db.mongo, user_id)
+    return db_user.get_user_by_id(db.mongo, user_id)
+
 
 @api_user.route('/user/get/token/<token>')
 def user_get_by_token(token):
-      return db_user.get_user_by_token(db.mongo, token)
-      
+    return db_user.get_user_by_token(db.mongo, token)
