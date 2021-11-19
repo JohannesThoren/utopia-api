d3d6afb03bf39b2df5f9673cfc9c2947a65156f4
Johannes Thorén <45944438+JohannesThoren@users.noreply.github.com>
2021-11-19 11:20:31 +0100

    added a function to get the n latest global posts

--- db_board_posts.py
+++ db_board_posts.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021 Johannes Thorén
-# 
+#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -9,72 +9,93 @@ from datetime import datetime
 import db_user
 
 from response_codes import *
+
+
 def new_post(mongo, board_id, token, content, title, flag="TEXT"):
-      db = mongo.db.posts
-      if db_user.auth_user(mongo, token):
-            user_id, username = db_user.auth_user(mongo, token)
+    db = mongo.db.posts
+    if db_user.auth_user(mongo, token):
+        user_id, username = db_user.auth_user(mongo, token)
 
-            # a post dict that will be used to add a new post to the database
-            new_post = {
-                  "title": title, 
-                  "content": content, 
-                  "board": UUID(board_id), 
-                  "created": datetime.now(), 
-                  "author": user_id, 
-                  "flag": flag,
-                  "id": uuid4(),
-                  # "likes": 0
-                  }
+        # a post dict that will be used to add a new post to the database
+        new_post = {
+            "title": title,
+            "content": content,
+            "board": UUID(board_id),
+            "created": datetime.now(),
+            "author": user_id,
+            "flag": flag,
+            "id": uuid4(),
+            # "likes": 0
+        }
 
-            db.insert(new_post)
-            return {"response code": OK, "msg": "post created"}
-      else:
-            return {"response code": NOT_AUTHORIZED}      
+        db.insert(new_post)
+        return {"response code": OK, "msg": "post created"}
+    else:
+        return {"response code": NOT_AUTHORIZED}
+
+
+def get_n_amount_of_latest_posts(mongo, n):
+    n = int(n)
+    db = mongo.db.posts
+    posts = db.find({})
+    post_dict = {}
+    index = 0
+
+    if n > posts.count():
+        n = posts.count()
+        
+    if posts:
+        for i in reversed(range(posts.count() - n, posts.count())):    
+            post = posts[i]        
+            post_dict.update({f"{index}": {"title": post["title"], "id": post["id"], "content": post["content"],
+                             "author": post["author"], "created": post["created"], "flag": post["flag"]}})
+            index += 1
+        return post_dict
 
 def get_specific_post(mongo, post_id):
-      db = mongo.db.posts
-      post = db.find_one({"id": UUID(post_id)})
-      if post:
-            return {
-                  "id": post["id"],
-                  "title": post["title"],
-                  "author": post["author"],
-                  "board": post["board"],
-                  "created": post["created"],
-                  "content": post["content"],
-                  "flag": post["flag"],
-                  "response code": OK
-                  }
-      else:
-            return {"response code": NOT_FOUND}
+    db = mongo.db.posts
+    post = db.find_one({"id": UUID(post_id)})
+    if post:
+        return {
+            "id": post["id"],
+            "title": post["title"],
+            "author": post["author"],
+            "board": post["board"],
+            "created": post["created"],
+            "content": post["content"],
+            "flag": post["flag"],
+            "response code": OK
+        }
+    else:
+        return {"response code": NOT_FOUND}
 
 
 def get_all_posts(mongo, board_id):
-      db = mongo.db.posts
-      posts = db.find({"board": UUID(board_id)})
-      post_dict = {}
-      index = 0
+    db = mongo.db.posts
+    posts = db.find({"board": UUID(board_id)})
+    post_dict = {}
+    index = 0
 
-      if posts:
-            for post in posts:
-                  post_dict.update({f"{index}":{"title": post["title"],"id": post["id"], "content": post["content"], "author": post["author"], "created": post["created"], "flag": post["flag"]}})
-                  index += 1
+    if posts:
+        for post in posts:
+            post_dict.update({f"{index}": {"title": post["title"], "id": post["id"], "content": post["content"],
+                             "author": post["author"], "created": post["created"], "flag": post["flag"]}})
+            index += 1
 
-            return post_dict
-      else:
-            return {"response code": NOT_FOUND, "msg": "there are currently no posts"}
+        return post_dict
+    else:
+        return {"response code": NOT_FOUND, "msg": "there are currently no posts"}
 
 
 def delete_post(mongo, post_id, token):
-      db = mongo.db.posts
-      post = db.find_one({"id": UUID(post_id)})
-      author_id, username = db_user.auth_user(mongo, token)
-      if post:
-            if post["author"] == author_id:
-                  db.delete_one(post)
-                  return {"response code": OK, "msg": "post deleted"}
-            else:
-                  return {"response code": NOT_AUTHORIZED}
-      else:
-            return {"response code": NOT_FOUND}
-                  
+    db = mongo.db.posts
+    post = db.find_one({"id": UUID(post_id)})
+    author_id, username = db_user.auth_user(mongo, token)
+    if post:
+        if post["author"] == author_id:
+            db.delete_one(post)
+            return {"response code": OK, "msg": "post deleted"}
+        else:
+            return {"response code": NOT_AUTHORIZED}
+    else:
+        return {"response code": NOT_FOUND}
--- routes_board_posts.py
+++ routes_board_posts.py
@@ -3,35 +3,49 @@ from flask_pymongo import PyMongo
 
 import json
 
-import db, db_user, db_boards, db_board_posts
+import db
+import db_user
+import db_boards
+import db_board_posts
 
 api_board_posts = blueprints.Blueprint('api_board_posts', __name__)
 
 # TODO All routess that should be a post request, should have all data in the body
 
+
 # TODO this should be a post request
 @api_board_posts.route('/board/<board_id>/post/new', methods=['POST'])
 def board_posts_new(board_id):
-      body = json.loads(request.get_data().decode("UTF-8"))
-      return db_board_posts.new_post(db.mongo, board_id, body["token"], body["content"], body["title"], body["flag"])
+    body = json.loads(request.get_data().decode("UTF-8"))
+    return db_board_posts.new_post(db.mongo, board_id, body["token"],
+                                   body["content"], body["title"],
+                                   body["flag"])
+
 
 @api_board_posts.route('/board/<board_id>/posts/get/all')
 def board_posts_all(board_id):
-      return db_board_posts.get_all_posts(db.mongo, board_id)
+    return db_board_posts.get_all_posts(db.mongo, board_id)
+
+
+@api_board_posts.route('/get/<n>/global/posts')
+def get_n_global_posts(n):
+    return db_board_posts.get_n_amount_of_latest_posts(db.mongo, n)
+
 
 @api_board_posts.route('/post/<post_id>')
 def board_posts_get_specific(post_id):
-      return db_board_posts.get_specific_post(db.mongo, post_id)
+    return db_board_posts.get_specific_post(db.mongo, post_id)
+
 
 # TODO this should be a post request
 @api_board_posts.route('/post/<post_id>/edit')
 def board_posts_specific_edit(board_id, post_id):
-      body = request.json
-      return
+    body = request.json
+    return
+
 
 # TODO this should be a post request
 @api_board_posts.route('/post/<post_id>/delete')
 def board_posts_specific_delete(post_id):
-      body = request.json
-      return db_board_posts.delete_post(db.mongo, post_id, body["token"])
-
+    body = request.json
+    return db_board_posts.delete_post(db.mongo, post_id, body["token"])
